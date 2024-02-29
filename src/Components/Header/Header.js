46a3301b67ede77e09a5a544097cdb9186498ba6
itsjustriley <142259664+itsjustriley@users.noreply.github.com>
2024-02-29 11:21:56 -0400

    tweaking header alt text

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="bg-light-purple flex justify-between items-center p-10 mb-10 sticky top-0">
       <div className="flex items-center">
-        <img className="mr-4" src="icons8-magic-book-60.png" alt="Magic Wand Icon" />
+        <img className="mr-4" src="icons8-magic-book-60.png" alt="Magic Book Icon" />
         <h1 className="text-xl text-purple font-bold">Spellbook Manager</h1>
       </div>
       <ul className="flex pr-10 text-medium-purple">
