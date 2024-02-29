5c32c99a289356ce3cc0a0ad3948bbd40ac7596c
itsjustriley <142259664+itsjustriley@users.noreply.github.com>
2024-02-29 10:02:37 -0400

    corrections

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,13 +9,13 @@ function Header() {
       </div>
       <ul className="flex pr-10 text-medium-purple">
         <li className="mr-10">
-          <a className={`hover:text-white`} href="#">Prepared Spells</a>
+          <a className={`hover:text-white`} href="/spellbook-manager">Prepared Spells</a>
         </li>
         <li className="mr-10">
-          <a className="hover:text-white" href="#known">Spells Known</a>
+          <a className="hover:text-white" href="/spellbook-manager#known">Spells Known</a>
         </li>
         <li>
-          <a className="hover:text-white" href="#search">Spell Search</a>
+          <a className="hover:text-white" href="/spellbook-manager#search">Spell Search</a>
         </li>
       </ul>
     </header>
